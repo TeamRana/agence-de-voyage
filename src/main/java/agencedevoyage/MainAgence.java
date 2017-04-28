d3d97ebf4d34427b5ad2699a2ad95752044409c2
anaislancry <anaislancry@yahoo.fr>
2017-04-28 11:32:02 +0200

    commit de test entre membres de l'équipe

--- src/main/java/agencedevoyage/MainAgence.java
+++ src/main/java/agencedevoyage/MainAgence.java
@@ -5,6 +5,8 @@ public class MainAgence {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Je suis contente de faire partie de ton équipe");
+		
 	}
 
 }
